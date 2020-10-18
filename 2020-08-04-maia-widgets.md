b9efdd9c2fb48d5d7270412c09ec2bc69b258d04
Reid McIlroy-Young <reidmcy@gmail.com>
2020-10-18 02:07:25 -0400

    Moving the code over from csslab site

---
+++ 2020-08-04-maia-widgets.md
@@ -0,0 +1,56 @@
+---
+layout: post
+title: Maia Widgets
+tags:
+  - Maia
+authors: Reid
+---
+
+<script src="assets/js/jquery-3.5.1.js"></script>
+
+<link rel="stylesheet" href="assets/js/chessboardjs/css/chessboard-1.0.0.css" />
+<script src="assets/js/chessboardjs/js/chessboard-1.0.0.js"></script>
+
+<link rel="stylesheet" href="assets/css/widget_boards.css" />
+
+## Disclaimer
+
+This interactive widget is intended to be a supplement and inline with our blog post. It is aimed at the general reader, who we assume knows a little bit about chess. It is also still in development, I'm not very good at web dev, so there will be UI issues. The colours are not final either and are one of the things I want feedback on.
+
+Also, the chess board is a fixed width `400px` so weird things will happen if you view on a narrow screen.
+
+## Quick summary
+
+This is to go with work I did late last year. The main result was a series of chess engines, we called Maia, that can predict the next move a player will make with decent accuracy. The full blog post can be fond [here](/blog/2020/08/24/maia_chess_kdd/)
+
+The main summary of the results is shown in figure 1
+
+{%- include blog-image.html  url="assets/images/all_lineplot.svg" caption="Move matching accuracy for all the models compared with the targeted player's ELO rating" -%}
+
+## Widget Intro
+
+While we have done some surface level exploration of the boards that Maia was tested, their sheer numbers make substantial human exploration impossible. This simple viewer alone has 174,248 separate boards. But we do expect some of them to interesting and hope that methods like ours can be used to explore the possibility space of chess.
+
+Below is a tool for exploring how our different models agree and disagree. All the boards and player moves are from real games on lichess, so you can select the approximate rating of both players, and if the next move made was a mistake.
+
+Then you can select which models correctly predicted the player's move by clicking on the underlined text. If `Force continuous` is enabled then only two options will be enabled, disable it to see any combination.
+
+There is also the option to scan through different boards with the same settings. The number of boards available varies depending on how unusual a model agreement matrix was selected. You can even find combinations with no examples (hint, mistakes stockfish guessed correctly).
+
+The info field is incomplete, and will probably be a collapsible element. The main interesting element is the link value which takes you to the game on lichess where the board is from.
+
+## Board Explore
+
+{% include_relative board-explorer.html data_path="data/base_data.json" %}
+
+## Game Explore
+
+{% include_relative game-explorer.html data_path="data/game_data.json" %}
+
+## What Maia Sees
+
+{% include_relative example.html %}
+
+Alternate view
+
+{% include_relative example2.html %}
---
+++ assets/js/board-explorer-backend.js
@@ -0,0 +1,229 @@
+targets = ['maia_1100', 'maia_1200', 'maia_1300', 'maia_1400', 'maia_1500', 'maia_1600', 'maia_1700', 'maia_1800', 'maia_1900', 'sf_m',]
+
+targets = []
+var i;
+for (i = 0; i < 5; i++) {
+    targets.push('maia_1' + ((i * 2) + 1) + '00')
+}
+targets.push('sf_m')
+
+var arrow_map_diagonal = {
+    true : {
+        true: '&#8601;',
+        false: '&#8600;',
+    },
+    false: {
+        true: '&#8598;',
+        false: '&#8599;',
+    }
+}
+
+var arrow_map_ortho = {
+    true: {
+        true: '&#8592;',
+        false: '&#8594;',
+    },
+    false: {
+        true: '&#8595;',
+        false: '&#8593;',
+    }
+}
+
+function get_url_parameter (url, name) {
+    var ret = url.searchParams.get(name);
+    if (ret == null) {
+        throw 'Missing parameter: ' + name;
+    }
+    return ret;
+}
+
+function setup_explorer_board(data_file) {
+    console.log("loading: " + data_file);
+
+    var url = new URL(window.location.href)
+    try {
+        for (i = 0; i < targets.length; i++) {
+
+            $('#' + targets[i] + '_toggle')[0].checked = get_url_parameter(url, targets[i]);
+            console.log(targets[i], get_url_parameter(url, targets[i]))
+        }
+        //$('#force_diagonal')[0].checked = get_url_parameter(url, "force_diagonal");
+        $('#is_blunder')[0].checked = get_url_parameter(url,"is_blunder");
+        $('#player_elo')[0].value = get_url_parameter(url,"player_rating");
+        $('#material_count')[0].value = get_url_parameter(url,"material_count");
+    } catch (err) {
+        console.log("errored", err)
+    }
+    $.getJSON(data_file, function(data) {
+        console.log("loaded: " + data_file);
+        all_boards = data;
+        update_explorer(false);
+    });
+}
+
+function update_explorer(complexity_changed) {
+    var i;
+    var s ='';
+    enabled_index_1 = 0;
+    enabled_index_2 = 4;
+    var disabled = true;//$('#force_diagonal')[0].checked;
+    var flip_encountered = false;
+    var start_value = $('#' + targets[0] + '_toggle')[0].checked;
+    for (i = 0; i < targets.length; i++) {
+        var c_val = $('#' + targets[i] + '_toggle')[0].checked;
+        if (c_val) {
+            s += '1'
+        } else {
+            s += '0'
+        }
+        if (! disabled) {
+            $('#' + targets[i] + '_toggle')[0].disabled = false;
+        } else if (i > 8) {
+            $('#' + targets[i] + '_toggle')[0].disabled = false;
+        } else if (c_val != start_value) {
+            if (flip_encountered) {
+                $('#' + targets[i] + '_toggle')[0].checked = ! start_value;
+            } else {
+                enabled_index_1 = i - 1;
+                enabled_index_2 = i;
+                flip_encountered = true;
+            }
+        }
+    }
+    if (disabled) {
+        for (i = 0; i < targets.length - 1; i++) {
+            if (i == enabled_index_1) {
+                $('#' + targets[i] + '_toggle')[0].disabled = false;
+            } else if (i == enabled_index_2) {
+                $('#' + targets[i] + '_toggle')[0].disabled = false;
+            } else {
+                $('#' + targets[i] + '_toggle')[0].disabled = true;
+            }
+        }
+    }
+    var is_blunder = $('#is_blunder')[0].checked;
+    var elo = $('#player_elo')[0].value;
+    console.log(s, elo, is_blunder);
+    if (!complexity_changed) {
+        try {
+            var dat = all_boards[s][elo][is_blunder];
+            var material_val = dat.length / 2
+            material_val = Math.floor(material_val)
+            $('#material_count')[0].min = 0;
+            $('#material_count')[0].max = dat.length - 1;
+            $('#material_count')[0].value = material_val;
+        } catch (err) {
+            material_val = 0
+            $('#material_count')[0].min = 0;
+            $('#material_count')[0].max = 0;
+            $('#material_count')[0].value = 0;
+        }
+
+    }
+    var material = $('#material_count')[0].value;
+
+    //console.log(s);
+
+    switch_to_board(s, elo, is_blunder, material);
+}
+
+function switch_to_board(board_str, player_elo, is_blunder, material_count) {
+    var player_move = "unknown"
+    try {
+        if (($('#' + targets[targets.length - 1] + '_toggle')[0].checked) & is_blunder) {
+            throw 'Stockfish incorrectly correct';
+        }
+        dat = all_boards[board_str][player_elo][is_blunder][material_count];
+        var board = Chessboard(
+            "explorer-board", {
+            position: dat['board'],
+            draggable: false,
+            sparePieces: false,
+            dropOffBoard: 'trash',
+        });
+        var i;
+        var square_size = calculateSquareSize()
+        for (i = 0; i < targets.length; i++) {
+            var e_move = dat[targets[i] + "_move"]
+            $("#" + targets[i] + "_move").html(e_move)
+
+            if (dat[targets[i] + "_correct"]) {
+                player_move = e_move;
+                $.find('#explorer-board .square-' + e_move.substring(0, 2))[0].className += ' highlight-correct'
+                $.find('#explorer-board .square-' + e_move.substring(2, 4))[0].className += ' highlight-correct'
+                /*$('#explorer-board .square-' + e_move.substring(2, 4)).append(
+                        '<svg width="' +
+                        square_size +
+                        'px" height="' +
+                    square_size + 'px"><circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" /></svg>')*/
+            } else {
+                //$.find('#explorer-board .square-' + e_move.substring(0, 2))[0].className += ' highlight-' + targets[i]
+                //$.find('#explorer-board .square-' + e_move.substring(2, 4))[0].className += ' highlight-' + targets[i]
+            }
+            var from_symbol = arrow_map_diagonal[e_move[1] > e_move[3]][e_move[0] > e_move[2]];
+
+            if (e_move[1] == e_move[3]) {
+                from_symbol = arrow_map_ortho[true][e_move[0] > e_move[2]];
+            } else if (e_move[0] == e_move[2]) {
+                from_symbol = arrow_map_ortho[false][e_move[1] > e_move[3]];
+            }
+
+            $('#explorer-board .square-' + e_move.substring(0, 2)).append('<div class="model-move-square-from ' + targets[i] + '_move_square">' + from_symbol+'</div>')
+            $('#explorer-board .square-' + e_move.substring(2, 4)).append('<div class="model-move-square-to ' + targets[i] + '_move_square"> &#9675; </div>')
+        }
+    } catch (err) {
+        dat = {};
+        for (i = 0; i < targets.length; i++) {
+            dat[targets[i] + "_move"] = "????";
+            dat[targets[i] + "_correct"] = false;
+            dat["game_id"] = "None";
+            dat["move_ply"] = 0;
+            dat["board"] = "";
+            dat['material_count'] = 0;
+            dat["count"] = 0;
+            dat["cp_rel"] = "";
+        }
+        $("#explorer-board").html("<h3>No Board with these properties was found in our dataset</h3>")
+    }
+
+    game_str = "lichess.org/" + dat["game_id"] + "#" + dat["move_ply"]
+    $("#game_link").html(
+        '<a href="https://' + game_str + '">' + game_str + "</a>"
+    )
+
+    $("#player_move").html('<span class="move_text">' + player_move + '</span>')
+    $("#rating").html(player_elo)
+    $("#fen").html('<span class="fen_text">' + dat["board"] + '</span>')
+    $("#count_string").text("There were " + dat["count"] + " boards, out of TODO, with this combination of models correct.")
+    $("#cp_string").text("The number of pawns the current player was advantaged by here is: " + dat["cp_rel"] + ".")
+    var url = new URL(window.location.href)
+    for (i = 0; i < targets.length; i++) {
+        url.searchParams.append(targets[i], $('#' + targets[i] + '_toggle')[0].checked);
+    }
+    url.searchParams.append("player_rating",player_elo);
+    url.searchParams.append("is_blunder", is_blunder);
+    url.searchParams.append("material_count", material_count);
+    //url.searchParams.append("force_diagonal", $('#force_diagonal')[0].checked);
+    $("#url").html(
+        '<a href="' + url + '">link</a>'
+    )
+}
+
+function calculateSquareSize() {
+    var container = $("#explorer-board")
+    var containerWidth = parseInt(container.width(), 10)
+
+    // defensive, prevent infinite loop
+    if (!containerWidth || containerWidth <= 0) {
+        return 0
+    }
+
+    // pad one pixel
+    var boardWidth = containerWidth - 1
+
+    while (boardWidth % 8 !== 0 && boardWidth > 0) {
+        boardWidth = boardWidth - 1
+    }
+
+    return boardWidth / 8
+}
