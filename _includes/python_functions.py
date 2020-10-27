1facb2dce55b049ddbc286d1552b74b9efb002c4
Reid McIlroy-Young <reidmcy@gmail.com>
2020-10-27 02:18:15 -0400

    lets try this

--- _includes/python_functions.py
+++ _includes/python_functions.py
@@ -7,10 +7,9 @@ def final_init():
 
 
 
-micropip.install('http://127.0.0.1:4000/Maia-Agreement-Visualizer/assets/python/chess-1.2.1-py3-none-any.whl').then(print("python chess loaded"))
+micropip.install("{{ "/assets/python/chess-1.2.1-py3-none-any.whl" | absolute_url }}").then(print("python chess loaded"))
 
-micropip.install('http://127.0.0.1:4000/Maia-Agreement-Visualizer/assets/python/leela_board-0.0.1-py3-none-any.whl').then(final_init())
-
+micropip.install("{{ "/assets/python/leela_board-0.0.1-py3-none-any.whl" | absolute_url }}").then(final_init())
 
 def get_board_array():
     import leela_board
