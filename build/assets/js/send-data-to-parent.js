5d143f3f52ba1be2b610fbf81edf20eee181223a
Reid McIlroy-Young <reidmcy@gmail.com>
2020-12-02 11:40:19 -0500

    MS magic

---
+++ build/assets/js/send-data-to-parent.js
@@ -0,0 +1,42 @@
+/**
+ * Handles communication to a parent container when this project is
+ * hosted within an iframe.
+ */
+
+(function() {
+    /**
+     * Handler to send height data to parent window.
+     * Useful for responsively sizing the iframe this content
+     * will be loaded to.
+     */
+    function sendContentDataToParent() {
+        var targetElementContainer = document.querySelector('.explorer');
+        var targetId = targetElementContainer.getAttribute('id');
+
+        if(!id || !id.length) {
+            targetId = 'msr-chess-experience';
+        }
+
+        var data = {};
+        if (targetElementContainer) {
+            data = {
+                height: targetElementContainer.offsetHeight,
+                id: targetId
+            };
+        }
+        window.parent.postMessage(data, '*');
+    }
+
+    // Init debouncer
+    var debouncer = null;
+
+    // Bind to load
+    window.parent.addEventListener('load', sendContentDataToParent);
+
+    // Bind to resize, debounce
+    window.addEventListener('resize', function() {
+        clearTimeout(debouncer);
+        debouncer = setTimeout(sendContentDataToParent, 300);
+    });
+}());
+
