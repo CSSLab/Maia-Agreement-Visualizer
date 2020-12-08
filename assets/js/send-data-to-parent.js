4b919cceb4de909e61e69c84960f49ec4e76c12b
Reid McIlroy-Young <reidmcy@gmail.com>
2020-12-08 16:43:03 -0500

    update

--- assets/js/send-data-to-parent.js
+++ assets/js/send-data-to-parent.js
@@ -3,40 +3,39 @@
  * hosted within an iframe.
  */
 
-function sendContentDataToParent() {
-    var targetElementContainer = document.querySelector('.explorer');
-    var targetId = targetElementContainer.getAttribute('id');
-
-    if (!targetId || !targetId.length) {
-        targetId = 'msr-chess-experience';
-    }
-
-    var data = {};
-    if (targetElementContainer) {
-        data = {
-            height: targetElementContainer.offsetHeight,
-            id: targetId
-        };
-    }
-    window.parent.postMessage(data, '*');
-}
-
-(function() {
+(function () {
     /**
      * Handler to send height data to parent window.
      * Useful for responsively sizing the iframe this content
      * will be loaded to.
      */
+    window.sendContentDataToParent = function sendContentDataToParent() {
+        var targetElementContainer = document.querySelector('.explorer');
+        var targetId = targetElementContainer.getAttribute('id');
+
+        if (!targetId || !targetId.length) {
+            targetId = 'msr-chess-experience';
+        }
+
+        var data = {};
+        if (targetElementContainer) {
+            data = {
+                height: targetElementContainer.offsetHeight,
+                id: targetId
+            };
+        }
+        window.parent.postMessage(data, '*');
+    }
+
     // Init debouncer
     var debouncer = null;
 
     // Bind to load
-    window.addEventListener('load', sendContentDataToParent);
+    window.addEventListener('load', window.sendContentDataToParent);
 
     // Bind to resize, debounce
-    window.addEventListener('resize', function() {
+    window.addEventListener('resize', function () {
         clearTimeout(debouncer);
-        debouncer = setTimeout(sendContentDataToParent, 300);
+        debouncer = setTimeout(window.sendContentDataToParent, 300);
     });
 }());
-
